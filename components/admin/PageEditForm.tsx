881ac95838a9dba3df77d1474257e34d97ad4fe8
Oliodellacontrada <oliodellacontrada@gmail.com>
2026-01-15 18:54:36 +0100

    Update PageEditForm.tsx

--- components/admin/PageEditForm.tsx
+++ components/admin/PageEditForm.tsx
@@ -20,9 +20,10 @@ export default function PageEditForm({ page }: { page: Page }) {
     setLoading(true)
 
     try {
+      // @ts-ignore
       const { error } = await supabase
         .from('pages')
-        .update({ title, content } as any)
+        .update({ title, content })
         .eq('id', page.id)
 
       if (error) throw error

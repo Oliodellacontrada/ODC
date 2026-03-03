cc9b5a1ed87dec3cbfd06a67d5d4458a4e3b3ce7
Oliodellacontrada <oliodellacontrada@gmail.com>
2026-03-03 15:28:45 +0100

    Update route.ts

--- app/admin/logout/route.ts
+++ app/admin/logout/route.ts
@@ -2,10 +2,11 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export async function POST() {
+export async function POST(request: Request) {
   const supabase = createRouteHandlerClient({ cookies })
   
   await supabase.auth.signOut()
   
-  return NextResponse.redirect(new URL('/admin/login', process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'))
+  const requestUrl = new URL(request.url)
+  return NextResponse.redirect(`${requestUrl.origin}/admin/login`)
 }
